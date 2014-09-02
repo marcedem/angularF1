d00bc07a05e32ade263fc44da02d5ada72b333db
edemguru <marcsea2000@gmail.com>
2014-09-02 04:31:23 +0200

    first commit, first leg of our application

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,27 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+// angular.module('myApp', [
+//   'ngRoute',
+//   'myApp.filters',
+//   'myApp.services',
+//   'myApp.directives',
+//   'myApp.controllers'
+// ]).
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+
+angular.module('F1FeederApp', [
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.
+	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+	otherwise({redirectTo: '/drivers'});
 }]);
